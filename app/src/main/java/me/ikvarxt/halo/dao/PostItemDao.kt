f5703ab4857fed1ccafeef1f4b6f779e803e456c
ikvarxt <leonekola@outlook.com>
2022-03-11 18:21:27 +0800

    [Feature]: 加载文章按照创建时间倒序排列

--- app/src/main/java/me/ikvarxt/halo/dao/PostItemDao.kt
+++ app/src/main/java/me/ikvarxt/halo/dao/PostItemDao.kt
@@ -10,7 +10,7 @@ import me.ikvarxt.halo.entites.PostItem
 @Dao
 interface PostItemDao {
 
-    @Query( "SELECT * from post_item")
+    @Query("SELECT * FROM post_item ORDER BY create_time DESC")
     fun loadAllPosts(): LiveData<List<PostItem>>
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)

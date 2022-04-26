d5caecf852e61022b3e61292062aad391755a656
ikvarxt <leonekola@outlook.com>
2022-04-26 17:46:35 +0800

    refactor: refactor main navigation logic and insets processing

--- app/src/main/java/me/ikvarxt/halo/ui/MainActivity.kt
+++ app/src/main/java/me/ikvarxt/halo/ui/MainActivity.kt
@@ -2,6 +2,8 @@ package me.ikvarxt.halo.ui
 
 import android.os.Bundle
 import androidx.core.view.forEach
+import androidx.core.view.isGone
+import androidx.core.view.isVisible
 import androidx.navigation.NavController
 import androidx.navigation.NavDirections
 import androidx.navigation.fragment.NavHostFragment
@@ -14,14 +16,59 @@ import me.ikvarxt.halo.databinding.ActivityMainBinding
 @AndroidEntryPoint
 class MainActivity : BaseActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+
+    private var isRootFragment = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-//        supportFragmentManager.commit { add<MainMainFragment>(android.R.id.content) }
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        setContentView(ActivityMainBinding.inflate(layoutInflater).root)
+        setSupportActionBar(binding.toolbar)
 
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.container) as NavHostFragment
+        navController = navHostFragment.navController
 
-        WindowCompat.setDecorFitsSystemWindows(window, false)
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            isRootFragment = when (destination.id) {
+                R.id.postsListFragment,
+                R.id.dashboardFragment -> true
+                else -> false
+            }
+
+            requestNavigationHidden(!isRootFragment)
+
+            binding.bottomNav.menu.forEach { item ->
+                if (item.itemId == destination.id) {
+                    item.isChecked = true
+                }
+            }
+        }
+
+        binding.bottomNav.setOnItemSelectedListener {
+            getPage(it.itemId)?.let { navController.navigate(it) }
+            true
+        }
+        binding.bottomNav.setOnItemReselectedListener { /* do nothing */ }
+    }
+
+    private fun getPage(destinationId: Int): NavDirections? = when (destinationId) {
+        R.id.postsListFragment -> MainDirections.actionPostsListFragment()
+        R.id.dashboardFragment -> MainDirections.actionDashboardFragment()
+        else -> null
+    }
+
+    private fun requestNavigationHidden(hide: Boolean = true, requiresAnimation: Boolean = true) {
+        val bottomView = binding.bottomNav
+        if (requiresAnimation) {
+            bottomView.isVisible = true
+            bottomView.isHidden = hide
+        } else {
+            bottomView.isGone = hide
+        }
     }
 }

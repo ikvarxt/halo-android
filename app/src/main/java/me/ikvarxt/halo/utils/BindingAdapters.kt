b711262785aed8793a2999af5de7662a6aaab691
ikvarxt <leonekola@outlook.com>
2022-05-10 15:58:10 +0800

    feat: add default error image to glide image loader

--- app/src/main/java/me/ikvarxt/halo/utils/BindingAdapters.kt
+++ app/src/main/java/me/ikvarxt/halo/utils/BindingAdapters.kt
@@ -1,12 +1,27 @@
 package me.ikvarxt.halo.utils
 
+import android.graphics.drawable.Drawable
 import android.widget.ImageView
+import androidx.core.content.res.ResourcesCompat
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
+import me.ikvarxt.halo.R
 
-@BindingAdapter("imageUrl")
-fun ImageView.bindImageWithUrl(url: String) {
-    Glide.with(this)
+@BindingAdapter(value = ["imageUrl", "error"], requireAll = false)
+fun ImageView.bindImageWithUrl(url: String?, error: Drawable?) {
+    var glide = Glide.with(this)
         .load(url)
-        .into(this)
+
+    glide = if (error != null) {
+        glide.error(error)
+    } else {
+        val errorDrawable = ResourcesCompat.getDrawable(
+            resources,
+            R.drawable.ic_baseline_error_24,
+            context.theme
+        )
+        glide.error(errorDrawable)
+    }
+
+    glide.into(this)
 }

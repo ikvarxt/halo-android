960e8d1fa47cd8455208bdf5554e3d283043bfe2
ikvarxt <leonekola@outlook.com>
2022-05-13 18:00:20 +0800

    fix: profile avatar load error
    caused by leaking "https:" schema in link

--- app/src/main/java/me/ikvarxt/halo/entites/UserProfile.kt
+++ app/src/main/java/me/ikvarxt/halo/entites/UserProfile.kt
@@ -8,8 +8,11 @@ data class UserProfile(
     val username: String,
     val description: String,
     val email: String,
-    val avatar: String,
+    private val avatar: String,
     val createTime: Calendar,
     val updateTime: Calendar,
     val mfaType: String,
-)
+) {
+    val avatarUrl: String
+        get() = "https:$avatar"
+}

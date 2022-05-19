1b177fa6403d42c14dbb32a7d0042c3638fa1a19
ikvarxt <leonekola@outlook.com>
2022-05-19 19:23:55 +0800

    refactor: remove DAO injection from DatabaseModule

--- app/src/main/java/me/ikvarxt/halo/di/DatabaseModule.kt
+++ app/src/main/java/me/ikvarxt/halo/di/DatabaseModule.kt
@@ -6,7 +6,6 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import me.ikvarxt.halo.application
-import me.ikvarxt.halo.dao.PostItemDao
 import me.ikvarxt.halo.database.HaloDatabase
 import javax.inject.Singleton
 
@@ -21,11 +20,4 @@ class DatabaseModule {
             application, HaloDatabase::class.java, "halo-db"
         ).build()
     }
-
-    @Provides
-    fun providePostItemDao(database: HaloDatabase): PostItemDao = database.postItemDao()
-
-    @Provides
-    fun providePostDetailDao(database: HaloDatabase) = database.postDetailsDao()
-
 }

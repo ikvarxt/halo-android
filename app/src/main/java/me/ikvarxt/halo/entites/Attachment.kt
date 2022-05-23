ce6f289facf8dd8e510f674f4377d56b27d022dc
ikvarxt <leonekola@outlook.com>
2022-05-23 10:17:23 +0800

    fix: attachments file path gets the correct way

--- app/src/main/java/me/ikvarxt/halo/entites/Attachment.kt
+++ app/src/main/java/me/ikvarxt/halo/entites/Attachment.kt
@@ -1,5 +1,7 @@
 package me.ikvarxt.halo.entites
 
+import com.google.gson.annotations.SerializedName
+import me.ikvarxt.halo.di.NetworkModule
 import java.util.*
 
 data class Attachment(
@@ -9,9 +11,26 @@ data class Attachment(
     val fileKey: String,
     val height: Int,
     val width: Int,
-    val path: String,
+    @SerializedName("path")
+    private val endPath: String,
     val size: Long,
     val suffix: String,
-    val thumbPath: String,
+    @SerializedName("thumbPath")
+    private val endThumbPath: String,
 //    val type
-)
+) {
+    val path: String
+        get() = if (isEndPointPath(endPath)) {
+            "${NetworkModule.PLACEHOLDER_DOMAIN}$endPath"
+        } else endPath
+
+    val thumbnailPath: String
+        get() = if (isEndPointPath(endThumbPath)) {
+            "${NetworkModule.PLACEHOLDER_DOMAIN}$endThumbPath"
+        } else endThumbPath
+
+    private fun isEndPointPath(path: String): Boolean {
+        return path.startsWith("/")
+    }
+}
+
